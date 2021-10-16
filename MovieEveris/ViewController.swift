b52bd72c8b0b2512b817fd04913a4e1796255686
AlexOrdayaF <aordayaf@everis.com>
2021-10-16 10:24:12 -0500

    Initial Commit

---
+++ MovieEveris/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieEveris
+//
+//  Created by Alex Percy Ordaya Flores on 16/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
